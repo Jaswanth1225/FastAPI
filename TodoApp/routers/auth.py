7fe25500f2e31334ec4c1ae6d8f33c8a0035ef6a
Jaswanth1225 <saijaswanth.paruchuri@gmail.com>
2026-02-02 10:36:25 +0530

    Used alembic to add and delete a column

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -28,6 +28,7 @@ class CreateUser(BaseModel):
     last_name : str
     password : str
     role : str
+    phone_number : str
 
 class Token(BaseModel):
     access_token : str
@@ -79,7 +80,8 @@ async def create_user(create_user_request: CreateUser, db: db_dependency):
         last_name = create_user_request.last_name,
         hashed_password = bcrypt_context.hash(create_user_request.password),
         role = create_user_request.role,
-        is_active = True
+        is_active = True,
+        phone_number = create_user_request.phone_number
     )
     db.add(create_user_model)
     db.commit()
